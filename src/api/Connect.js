524df4ba7cbc0cdc95f92391be8715d56351bf0d
Paul Zamshin <p.zamshin@mobility.hyundai.ru>
2022-11-17 17:01:05 +0900

    срфтпув ифыуГкд

--- src/api/Connect.js
+++ src/api/Connect.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import store from '../store/configureStore';
 
-const baseUrl = 'https://komponent-app.ru/api/';
+// const baseUrl = 'https://komponent-app.ru/api/';
+const baseUrl = 'http://komponent-nano.ru';
 
 export const authUser = async params => {
     try {
